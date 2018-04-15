23e8e38ecb0a0203a74ba7b27f215da611c9205b
Pierre Macedo <pierre.alexander.mg@gmail.com>
2018-04-15 20:52:59 -0300

    Updated

--- js/tarot.js
+++ js/tarot.js
@@ -10,10 +10,7 @@ function spreadSetting(value){
 
 $(document).ready(function(){
         $("#menu").load("components/menu.html");
-});
-
-$( "#menu" ).click(function() {
-    $( ".menu-div" ).toggle();
+        $("#warning").load("components/warning.html");    
 });
 
 // Opening screen
@@ -27,13 +24,15 @@ $('#first-screen').addClass('display');
 
 // Reading starts here
 function thirdScreen(arg) {
+window.scrollTo(0, 0);    
 if (arg === "newq"){
 number = "";    
 }    
-document.getElementById("div-content").innerHTML='<div class="instruction-title">First Instructions</div><div class="first-instructions"><p class="item1">Think about what you wanna ask and formulate a clear question.</p><p class="item2">While focusing on your question, choose a number between 1 and 9.</p><p class="item3">In the next step you must choose three or five cards, whatever cards you want because there aren\'t wrong choices.</p><p style="text-align:center; margin-top: 20px;">Let\'s start? Choose a number.</p><p style="text-align:center;"><br><button id="1" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">1</button><button id="2" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">2</button><button id="3" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">3</button><button id="4" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">4</button><button id="5" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">5</button><button id="6" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">6</button><button id="7" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">7</button><button id="8" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">8</button><button id="9" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">9</button></p><div>';
+document.getElementById("div-content").innerHTML='<div class="instruction-title">First Instructions</div><div class="first-instructions"><p class="item1">Think about what you wanna ask and formulate a clear question.</p><p class="item2">While focusing on your question, choose a number between 1 and 9.</p><p class="item3">In the next step you must choose three or five cards, whatever cards you want because there aren\'t wrong choices.</p><p style="text-align:center; margin-top: 20px;">Let\'s start? Choose a number.</p><p style="text-align:center;margin-top: -20px;"><br><button id="1" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">1</button><button id="2" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">2</button><button id="3" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">3</button><button id="4" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">4</button><button id="5" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">5</button><button id="6" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">6</button><button id="7" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">7</button><button id="8" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">8</button><button id="9" type="button" class="btn btn-outline-info btn-lg" onclick="getId(this.id)">9</button></p><div>';
 }
 function getId(id){
     number = id;
+    $('#second-screen').removeClass('second-screen-main'); //remove margin-top to keep shuffle.gif centralized 
     document.getElementById("div-content").innerHTML='<div id="shuffle"><img class="shuffling" src="images/shuffle.gif" /></div>';
     cards = [];
     getData();
@@ -55,7 +54,8 @@ function getData(){
             changeN = "three";     
             }else {
               changeN = "five";  
-            }
+            }   
+        $('#second-screen').addClass('second-screen-main');   
         document.getElementById("div-content").innerHTML='<div id="show-cards"><div id="show-cards-top"><p style="font-size: calc(20px + 1.5vw);text-align:center;margin-top: 10px;margin-bottom: 10px">Choose ' + changeN + ' cards</p></div><div id="cards"></div><div><button type="button" class="btn btn-outline-primary" style="margin-top: 10px;color:#ffffff" id="newq" onclick="thirdScreen(this.id);">Ask another question</button></div></div>';   
         showCards();
     }, number + [100 + 10]);    
@@ -99,9 +99,9 @@ $('#' + id).addClass('selected');
 if (cards.length === parseInt(spread, 10)){ 
 for (let j = 0; j < parseInt(spread, 10); j++) { 
 if (parseInt(spread, 10) === 3){ // Check which spread is being used  
-showImg = '<img class="spread" src="images/' + cards[j] + '.jpg" id="' + cards[j] + '1"  onclick="showText(this.id)">';     
+showImg = '<img class="spread" src="images/' + cards[j] + '.jpg" id="' + cards[j] + '1"  onclick="showModal(this.id)">';     
 }else {
-showImg = '<img class="spread2" src="images/' + cards[j] + '.jpg" id="' + cards[j] + '1" onclick="showText(this.id)">';         
+showImg = '<img class="spread2" src="images/' + cards[j] + '.jpg" id="' + cards[j] + '1" onclick="showModal(this.id)">';         
 }
     $('#cards').append(showImg);
     
@@ -118,10 +118,11 @@ let five = '<p class="item1" style="font-size:calc(15px + 0.5vw);margin-top: 30p
     }else {
     $('#show-cards-top').html(five);    
     }
+    window.scrollTo(0, 0);
     
     // Generate modals for card explanations
     for (let i = 0; i < 22; i++) {
-        let text = '<div id="' + data[i]['id'] + '12" class="tarotModal"><div class="tarotModal-content"><div class="tarotModal-header">' + data[i]['name'] + '<span class="tarotClose" onclick="hideText(\'' + data[i]['id'] + '\')"><img src="images/close.png" /></span></div><div class="tarotModal-text">' + data[i]['meaning'] + '</div><div style="text-align:right;font-size:calc(12px + 0.5vw);color: #000000;margin: 0px 10px 10px 0px">Source: biddytarot.com</div></div></div>';
+        let text = '<div class="modal fade" id="' + data[i]['id'] + '12" tabindex="-1" role="dialog" aria-labelledby="warningModalCenterTitle" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" role="document"><div class="modal-content" style="font-family: "Stylish", sans-serif;"><div class="modal-header"><h5 class="modal-title" style="color:#000000;" id="warningModalLongTitle">' + data[i]['name'] + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="hideModal()"><span aria-hidden="true">&times;</span></button></div><div class="modal-body" style="text-align:justify;font-size:calc(12px + 0.5vw);color: #000000;">' + data[i]['meaning'] + '<div style="text-align:right;font-size:calc(12px + 0.5vw);color: #000000;margin: 0px 10px 10px 0px">Source: biddytarot.com</div></div></div></div></div>';
         $('#div-content').append(text);
         
         
@@ -131,25 +132,25 @@ let five = '<p class="item1" style="font-size:calc(15px + 0.5vw);margin-top: 30p
 }
 
 // Show modals for cards explanation
-function showText(id){
-document.getElementById(id + "2").style.display = "block"; 
-$('body').toggleClass("no-scroll");    
+function showModal(id){
+$('#' + id + '2').modal({backdrop: 'static', keyboard: false});  
+$('body').addClass("no-scroll");    
 }
-
-function hideText(id) {
-   document.getElementById(id + "12").style.display = "none";  
+function hideModal() {
 $('body').removeClass("no-scroll");    
 }
-
 // Help section
 function showHelp(arg) {
     if (arg === 'help'){
+window.scrollTo(0, 0);        
 controlback = true;        
 $('#content').addClass('display');
+$('#help').addClass('display');        
 $('#div-help').removeClass('display').load("components/help.html");    
         }
- if (arg === "outhelp"){ 
+ if (arg === "outhelp"){     
 $('#content').removeClass('display');
+$('#help').removeClass('display');      
 $('#div-help').addClass('display');     
  }    
 }
@@ -162,15 +163,20 @@ function onDeviceReady(){
 }
 function backKeyDown(answer) { 
     if (controlback === false){
-    $('#warning').dialog({dialogClass: "no-close"});    
+    $('#warning').removeClass("display");    
+    $('#warningModal').modal({backdrop: 'static', keyboard: false});
+    $('body').addClass("no-scroll");      
     }
     if (controlback === true){
+    window.scrollTo(0, 0);    
     $('#content').removeClass('display');
+    $('#help').removeClass('display');    
     $('#div-help').addClass('display');  
     controlback = false;    
     }
     if (answer === "no"){
-    $( "#warning" ).dialog( "close" );    
+    $( "#warningModal" ).modal( "toggle" );
+    $('body').removeClass("no-scroll");      
     }
 }
 function exitApp() {
